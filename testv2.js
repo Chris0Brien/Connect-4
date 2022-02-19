4e25b1444bcf0ad2315dc8dc9605d2d9f446588d
Chris0Brien <92539442+Chris0Brien@users.noreply.github.com>
2022-02-19 13:21:32 -0600

    Add files via upload
    made color swap and updated divs

---
+++ testv2.js
@@ -0,0 +1,81 @@
+class Connect4 {
+  constructor(selector) {
+    this.ROWS = 6;
+    this.COLS = 7;
+    this.player = "red";
+    this.selector = selector;
+    this.isGameOver = false;
+    this.onPlayerMove = function () {};
+    this.createGrid();
+    this.setupEventListeners();
+  }
+
+  createGrid() {
+    const $board = $(this.selector);
+    $board.empty();
+    this.isGameOver = false;
+    this.player = "red";
+    for (let row = 0; row < this.ROWS; row++) {
+      const $row = $("<div>").addClass("row");
+      for (let col = 0; col < this.COLS; col++) {
+        const $col = $("<div>")
+          .addClass("col empty")
+          .attr("data-col", col)
+          .attr("data-row", row);
+        $row.append($col);
+      }
+      $board.append($row);
+    }
+  }
+
+  setupEventListeners() {
+    const $board = $(this.selector);
+    const that = this;
+
+    function findLastEmptyCell(col) {
+      const cells = $(`.col[data-col='${col}']`);
+      for (let i = cells.length - 1; i >= 0; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass("empty")) {
+          return $cell;
+        }
+      }
+      return null;
+    }
+
+    $board.on("mouseenter", ".col.empty", function () {
+      if (that.isGameOver) return;
+      const col = $(this).data("col");
+      const $lastEmptyCell = findLastEmptyCell(col);
+      $lastEmptyCell.addClass(`next-${that.player}`);
+    });
+
+    $board.on("mouseleave", ".col", function () {
+      $(".col").removeClass(`next-${that.player}`);
+    });
+
+    $board.on("click", ".col.empty", function () {
+      if (that.isGameOver) return;
+      const col = $(this).data("col");
+      const $lastEmptyCell = findLastEmptyCell(col);
+      $lastEmptyCell.removeClass(`empty next-${that.player}`);
+      $lastEmptyCell.addClass(that.player);
+      $lastEmptyCell.data("player", that.player);
+
+      const winner = that.checkForWinner(
+        $lastEmptyCell.data("row"),
+        $lastEmptyCell.data("col")
+      );
+      if (winner) {
+        that.isGameOver = true;
+        alert(`Game Over! Player ${that.player} has won!`);
+        $(".col.empty").removeClass("empty");
+        return;
+      }
+
+      that.player = that.player === "red" ? "black" : "red";
+      that.onPlayerMove();
+      $(this).trigger("mouseenter");
+    });
+  }
+}
